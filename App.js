8b69d87c2a37fbd2cf3d0916062c9dee6e12b865
eunjungleecub <asaprocky123@likelion.org>
2019-10-30 21:58:41 +0900

    Create Components

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.textcontainer}>Open up App.js to start working on your app!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textcontainer:{
+    borderWidth:5;
+  }
+});
